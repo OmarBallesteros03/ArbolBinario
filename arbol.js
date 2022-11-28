f585a27ec7628d830138d86f6958e2e805c4da26
Omar Ballesteros <oballesteros@ucol.mx>
2022-11-28 00:53:08 -0600

    generar arbol

--- arbol.js
+++ arbol.js
@@ -1,89 +1,37 @@
-class ArbolBinario {
-    constructor(){
-        this.raiz=null;
-    }
-    agregar(nuevo){
-        if (this.raiz=null){
-            this.raiz=nuevo;
-        }else{
-            this._agregar(nuevo,this.raiz);
-        }
-    }
-    
-_agregar(nuevo, nodoX){
-        if(nuevo.dato<nodoX.dato){
-            if(nodoX.izq==null){
-                nodoX.izq=nuevo;
-            }else{
-                this._agregar(nuevo,nodoX.izq);
-            }
-        }else{
-            if(nodoX.der==null){
-                nodoX.der=nuevo;
-            }else{
-                this._agregar(nuevo,nodoX.der);
-            }
-        }
-    }
-    
-}
-
 class Nodo {
     constructor(dato){
         this.dato=dato;
         this.hder=null;
         this.hizq=null;
-
     }
 
+}
 
-
-
-    inOrder(){
-        if(this.raiz==null){
-            console.log(".");
-        }else{
-            _inOrderRec(this.raiz);
-        }
-    }
-    _inOrderRec(nodoX){
-        if(nodoX.izq!=null)
-           this._inOrderRec(nodoX.hizq);
-           console.log(nodoX.dato);
-        
-        if(nodoX.der!=null)
-            this._inOrderRec(nodoX.hder);
-            console.log(nodoX.dato);
-        
+class ArbolBinario {
+    constructor(){
+        this.raiz=null;
     }
 
-    preOrder(){
-        if(this.raiz==null){
-            return;
-        }else{
-            _preOrderRec(this.raiz);
+    generarArbol(expresion){
+        let vector = new Array;
+        for(let i=0; i<expresion.length; i++){
+            let cifra=expresion[i];
+            if(cifra === '+' || cifra === '-' || cifra === '*' || cifra === '/'){
+                let nodo = new Nodo(cifra);
+                nodo.der = vector.pop();
+                nodo.izq = vector.pop();
+                vector.push(nodo);
+            } else {
+                let nodo = new Nodo(cifra);
+                vector.push(nodo);
+            }
         }
-    }
-    _preOrderRec(nodoX){
-        let preString= ""
-        
-        preString=" "+nodoX+" "
-
-        if(nodoX.hizq!=null){
-            preString+=this.preOrder(nodoX.hizq)
-        }
-
-        if(nodoX.hder != null){
-            preString+=this.preOrder(nodoX.hder)
-        }
-
-        return preString
+        this.raiz = vector.pop();
+        return vector;
     }
 }
 
+
+
 let arbol = new ArbolBinario();
-let nodo = new Nodo(30);
-arbol.agregar(nodo);
-
-
-arbol.preOrder();
+console.log(arbol.generarArbol("3+5*8-6/2"));
